55eefb301e41d6dd6a34bcecd7fb842b8201b9e9
nandeshboyz024 <114298732+nandeshboyz024@users.noreply.github.com>
2024-10-14 16:31:53 +0530

    Update server.py

--- Emotion Detection/backend/server.py
+++ Emotion Detection/backend/server.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 global mydata
 mydata=[]
-global log_reg
+global rf_model
 
 
 def dataframe_difference(df1, df2, which=None):
@@ -61,6 +61,7 @@ def normalized_sentence(sentence):
     return sentence
 
 def train_model(model,data,targets):
+    # Term Frequency-Inverse Document Frequency (TF-IDF)
     text_clf=Pipeline([('vect',TfidfVectorizer()),('clf',model)])
     text_clf.fit(data,targets)
     return text_clf
@@ -74,8 +75,8 @@ def trainModel():
           train=normalize_text(train)
           X_train=train['text'].values
           y_train=train['emotion'].values
-          global log_reg
-          log_reg=train_model(RandomForestClassifier(random_state=0),X_train,y_train)
+          global rf_model
+          rf_model=train_model(RandomForestClassifier(random_state=0),X_train,y_train)
           return {'success':True}
      except:
           return {'success':False}
@@ -88,11 +89,11 @@ def predict_emotion():
          chances = [0,0,0,0,0,0]
          data = request.get_json()
          query = data['query']
-         global log_reg
-         predict_arr=log_reg.predict([normalized_sentence(query)])
-         predict_proba_arr=log_reg.predict_proba([normalized_sentence(query)])
+         global rf_model
+         predict_arr=rf_model.predict([normalized_sentence(query)])
+         predict_proba_arr=rf_model.predict_proba([normalized_sentence(query)])
          pred=predict_arr[0]
-         for emotion,probability in zip(log_reg.classes_,predict_proba_arr[0]):
+         for emotion,probability in zip(rf_model.classes_,predict_proba_arr[0]):
             if(emotion=='joy'):
                 chances[0]=round(probability*100)
             elif(emotion=='sadness'):
@@ -177,60 +178,6 @@ def getSize():
                'count':size,
           }
           return jsonify(response)
-
-# Text Emotion Custom
-import joblib
-
-qChoices=[
-    ('0','select query'),
-    ('1','I feel blessed to know this family'),
-    ('2','I am most defensive when I feel most threatened'),
-    ('3','What the hell is going on'),
-    ('4','I still feel horrible'),
-    ('5','I feel less threatened by the world'),
-    ('6','I am feeling a bit cranky today'),
-    ('7','I feel I have been loyal for my friend'),
-    ('8','I am feeling quite agitated irritated and annoyed'),
-    ('9','I feel like I am single handedly supporting the cupcake industry'),
-    ('10','I am feeling so nothing that I am not even getting agitated anymore')
-]
-
-
-@app.route('/predict-emotion-custom',methods=['POST'])
-def predict_emotion_custom():
-    try:
-         chances = [0,0,0,0,0,0]
-         data = request.get_json()
-         query = dict(qChoices)[data['query']]
-         dataset = data['dataset']
-         if(dataset=='1'):
-               loaded_model=joblib.load('pre_train_model1.joblib')
-         else:
-              loaded_model=joblib.load('pre_train_model2.joblib')
-         predict_arr=loaded_model.predict([query])
-         predict_proba_arr=loaded_model.predict_proba([query])
-         pred=predict_arr[0]
-         for emotion,probability in zip(loaded_model.classes_,predict_proba_arr[0]):
-            if(emotion=='joy'):
-                chances[0]=round(probability*100)
-            elif(emotion=='sadness'):
-                chances[1]=round(probability*100)
-            elif(emotion=='anger'):
-                chances[2]=round(probability*100)
-            elif(emotion=='fear'):
-                chances[3]=round(probability*100)
-            elif(emotion=='love'):
-                chances[4]=round(probability*100)
-            elif(emotion=='surprise'):
-                chances[5]=round(probability*100)
-         response={
-              'prediction':pred,
-              'chances':chances
-		 }   
-         return jsonify(response)
-    except Exception as e:
-         return jsonify({'error':str(e)})
-
 
 
 if __name__ == '__main__':
