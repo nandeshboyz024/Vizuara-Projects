87a53688e242e68eb770cdaaa3627ce3882b15ec
nandeshboyz024 <114298732+nandeshboyz024@users.noreply.github.com>
2024-10-20 11:43:34 +0530

    Update server.py

--- Emotion Detection/backend/server.py
+++ Emotion Detection/backend/server.py
@@ -11,17 +11,7 @@ mydata=[]
 global rf_model
 
 
-def dataframe_difference(df1, df2, which=None):
-    comparison_df = df1.merge(
-        df2,
-        indicator=True,
-        how='outer'
-    )
-    if which is None:
-        diff_df = comparison_df[comparison_df['_merge'] != 'both']
-    else:
-        diff_df = comparison_df[comparison_df['_merge'] == which]
-    return diff_df
+
 
 def Removing_numbers(text):
     text=''.join([i for i in text if not i.isdigit()])
